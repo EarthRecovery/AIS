599b5d18d657078cebbad5b357d2acf9b8f98cb7
root <root@KleinKaslana.localdomain>
2025-12-18 13:01:04 -0600

    cicd test

--- app/main.py
+++ app/main.py
@@ -12,7 +12,10 @@ app = FastAPI(title="Minimal Layered FastAPI")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://18.170.57.90:5173",
+        "http://localhost:5173",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
