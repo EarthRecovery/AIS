1e5f682da223a4487ab33976373600a33a69b206
root <root@KleinKaslana.localdomain>
2025-12-28 21:40:51 -0600

    finish login/register and resistence

--- frontend/src/utils/request.js
+++ frontend/src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const request = axios.create({
-  baseURL: 'http://localhost:8000', 
+  baseURL: 'http://18.170.57.90:8000',
   // baseURL: 'http://localhost:2222',
   timeout: 10000
 })
